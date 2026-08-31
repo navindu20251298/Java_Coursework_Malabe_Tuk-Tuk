17fd479e6a87ae3579ced6b42435298d741029ce
Navindu <navindu.20251298@iit.ac.lk>
2026-08-31 16:26:28 +0530

    add core fields to Dealer class

--- src/main/java/com/example/java_coursework_malabe_tuktuk/model/Dealer.java
+++ src/main/java/com/example/java_coursework_malabe_tuktuk/model/Dealer.java
@@ -1,4 +1,8 @@
 package com.example.java_coursework_malabe_tuktuk.model;
 
 public class Dealer {
+    private String dealerId;
+    private String name;
+    private String contact;
+    private String location;
 }
