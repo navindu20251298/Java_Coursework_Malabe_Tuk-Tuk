a095b05ff34e6553a92b5a258123d9bdf1037ed5
Navindu <navindu.20251298@iit.ac.lk>
2026-08-31 10:04:48 +0530

    add Part model class to encapsulate spare part details

--- src/main/java/com/example/java_coursework_malabe_tuktuk/model/Part.java
+++ src/main/java/com/example/java_coursework_malabe_tuktuk/model/Part.java
@@ -1,4 +1,15 @@
 package com.example.java_coursework_malabe_tuktuk.model;
 
+import java.time.LocalDate;
+
 public class Part {
+    private String partCode;
+    private String name;
+    private String brand;
+    private String category;
+    private double price;
+    private int quantity;
+    public LocalDate addDate;
+    private int threshold;
+    private String imagePath;
 }
