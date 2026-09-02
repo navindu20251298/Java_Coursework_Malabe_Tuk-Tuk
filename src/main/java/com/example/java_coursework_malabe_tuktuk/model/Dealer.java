4938739ed6c59f7e1062166e2609751e328534d5
Navindu <navindu.20251298@iit.ac.lk>
2026-09-02 23:28:59 +0530

    implement custom toString representation for Dealer

--- src/main/java/com/example/java_coursework_malabe_tuktuk/model/Dealer.java
+++ src/main/java/com/example/java_coursework_malabe_tuktuk/model/Dealer.java
@@ -26,4 +26,9 @@ public class Dealer {
     public String getLocation() {
         return location;
     }
+
+    @Override
+    public String toString() {
+        return dealerId + " - " + name + " (" + location + ") - " + contact;
+    }
 }
