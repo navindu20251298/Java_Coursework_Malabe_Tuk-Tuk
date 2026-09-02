af357f5c617ebb901d1b755e2f684a50e4628baa
Navindu <navindu.20251298@iit.ac.lk>
2026-09-03 00:17:30 +0530

    add items, totals, and timestamp attributes

--- src/main/java/com/example/java_coursework_malabe_tuktuk/model/Transaction.java
+++ src/main/java/com/example/java_coursework_malabe_tuktuk/model/Transaction.java
@@ -1,4 +1,12 @@
 package com.example.java_coursework_malabe_tuktuk.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Transaction {
+    private List<CartItem> items;
+    private double subtotal;
+    private double discountApplied;
+    private double total;
+    private LocalDateTime timestamp;
 }
