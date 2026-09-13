d9de4fd24a34cc0a26eeaa3793c513e7757fda06
Navindu <navindu.20251298@iit.ac.lk>
2026-09-13 15:14:03 +0530

    declare DealerSelector class with Random generator instance

--- src/main/java/com/example/java_coursework_malabe_tuktuk/logic/DealerSelector.java
+++ src/main/java/com/example/java_coursework_malabe_tuktuk/logic/DealerSelector.java
@@ -1,4 +1,10 @@
 package com.example.java_coursework_malabe_tuktuk.logic;
 
+import com.example.java_coursework_malabe_tuktuk.model.Dealer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class DealerSelector {
-}
+
+    private Random random = new Random();
