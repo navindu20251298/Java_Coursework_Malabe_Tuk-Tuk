62318e90ca4592a730de080c2ca29a5a8be5b1d4
Navindu <navindu.20251298@iit.ac.lk>
2026-09-13 15:12:49 +0530

    implement LowStockMonitor to identify items below safety threshold

--- src/main/java/com/example/java_coursework_malabe_tuktuk/logic/LowStockMonitor.java
+++ src/main/java/com/example/java_coursework_malabe_tuktuk/logic/LowStockMonitor.java
@@ -1,4 +1,20 @@
 package com.example.java_coursework_malabe_tuktuk.logic;
 
+import com.example.java_coursework_malabe_tuktuk.model.Part;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LowStockMonitor {
+
+    public List<Part> findLowStockItems(List<Part> parts) {
+        List<Part> lowStockItems = new ArrayList<>();
+
+        for (Part p : parts) {
+            if (p.getQuantity() < p.getThreshold()) {
+                lowStockItems.add(p);
+            }
+        }
+
+        return lowStockItems;
+    }
 }
